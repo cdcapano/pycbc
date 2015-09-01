63ddbf0aa32ca76dc27284b52972e7409b471679
Collin Capano <collin.capano@ligo.org>
2015-09-01 17:18:19 +0200

    added hdcoinc and ligolw support packages for lscarrays

--- pycbc/io/__init__.py
+++ pycbc/io/__init__.py
@@ -1,3 +1,5 @@
 from pycbc.io.sqlite import *
 from pycbc.io.hdf import *
-
+from pycbc.io import hdfcoinc
+from pycbc.io import ligolw
+known_pipelines = ['hdfcoinc']#, 'gstlal']
--- setup.py
+++ setup.py
@@ -446,6 +446,8 @@ setup (
                'pycbc.workflow',
                'pycbc.results',
                'pycbc.io',
+               'pycbc.io.ligolw',
+               'pycbc.io.hdfcoinc',
                ],
      package_data = {'pycbc.workflow': find_package_data('pycbc/workflow'), 
 	             'pycbc.results': find_package_data('pycbc/results'),
