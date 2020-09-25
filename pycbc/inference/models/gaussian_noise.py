ffb76d8e37194cbdb616139444126add2c1b8154
Shilpa Kastha <shilpakastha@aeiadminsmbp3.localdomain>
2020-09-25 13:34:03 +0200

    added the correct invspsd code

--- pycbc/inference/models/gaussian_noise.py
+++ pycbc/inference/models/gaussian_noise.py
@@ -1152,6 +1152,8 @@ class GatedGaussianNoise(BaseGaussianNoise):
             recalibration=self.recalibration,
             gates=self.gates, **self.static_params)
 
+        self._invpsd = {}
+
     @property
     def _extra_stats(self):
         """Adds ``loglr``, plus ``cplx_loglr`` and ``optimal_snrsq`` in each
@@ -1170,6 +1172,27 @@ class GatedGaussianNoise(BaseGaussianNoise):
             # snr can't be < 0 by definition, so return 0
             setattr(self._current_stats, '{}_optimal_snrsq'.format(det), 0.)
         return -numpy.inf
+
+
+    @property
+    def _invpsds(self):
+        """
+        Parameters
+        ----------
+        det : str
+            The name of the detector.
+        """
+        for det, d in self._data.items():
+            p = self.psds[det]
+            #print(p)
+            kmin = self._kmin[det]
+            kmax = self._kmax[det]
+            invp = FrequencySeries(numpy.zeros(len(p)), delta_f=p.delta_f)
+            invp[kmin:kmax] = 1./p[kmin:kmax]
+            self._invpsd[det] = invp
+        return self._invpsd
+
+
 
     def _loglr(self):
         r"""Computes the log likelihood ratio after removing the power
@@ -1203,8 +1226,7 @@ class GatedGaussianNoise(BaseGaussianNoise):
                 raise e
         lr = 0.
         for det, h in wfs.items():
-            #h._epoch = self._epoch
-            Invpsd=1. / self.psds[det][self._kmin[det]:-1]
+            Invpsds = self._invpsds[det]
             Det = Detector(det)
             #Accounting for the time delay between the waveforms of the different detectors
             gatestartdelay = gatestart + Det.time_delay_from_earth_center(self.current_params['ra'], self.current_params['dec'], gatestart)
@@ -1212,6 +1234,7 @@ class GatedGaussianNoise(BaseGaussianNoise):
             dgatedelay = gateenddelay - gatestartdelay
             # the kmax of the waveforms may be different than internal kmax
             kmax = min(len(h), self._kmax[det])
+            slc = slice(self._kmin[det], kmax)
             if self._kmin[det] >= kmax:
                 # if the waveform terminates before the filtering low frequency
                 # cutoff, then the loglr is just 0 for this detector
@@ -1219,9 +1242,10 @@ class GatedGaussianNoise(BaseGaussianNoise):
                 hh = 0.
             else:
                 #time series of the signal
-                H = h[self._kmin[det]:-1].to_timeseries()
+                h.resize(len(Invpsds))
+                H = h.to_timeseries()
                 #data details
-                d = self._data[det][self._kmin[det]:-1]
+                d = self._data[det]
                 D = d.to_timeseries()
                 D.resize(len(H))
 
@@ -1229,18 +1253,18 @@ class GatedGaussianNoise(BaseGaussianNoise):
 
 
                 ##Applying the gate method "paint"
-                gatedH = H.gate(gatestartdelay + dgatedelay/2, window=dgatedelay/2, copy=False, invpsd=Invpsd, method='paint')
-                gatedD = D.gate(gatestartdelay + dgatedelay/2, window=dgatedelay/2, copy=False, invpsd=Invpsd, method='paint')
+                gatedH = H.gate(gatestartdelay + dgatedelay/2, window=dgatedelay/2, copy=False, invpsd=Invpsds, method='paint')
+                gatedD = D.gate(gatestartdelay + dgatedelay/2, window=dgatedelay/2, copy=False, invpsd=Invpsds, method='paint')
 
                 ##conversion to the frequency series
                 gatedHFreq = gatedH.to_frequencyseries()
                 gatedDFreq = gatedD.to_frequencyseries()
                 #Normalization
-                gatedDFreq *= self._weight[det][self._kmin[det]:-1]
-                gatedHFreq *= self._weight[det][self._kmin[det]:-1]
+                gatedDFreq *= self._weight[det]
+                gatedHFreq *= self._weight[det]
                 #inner product
-                cplx_hd = gatedHFreq[self._kmin[det]:-1] . inner(gatedDFreq[self._kmin[det]:-1])  # <h, d>
-                hh = gatedHFreq[self._kmin[det]:-1] . inner(gatedHFreq[self._kmin[det]:-1]).real  # < h, h>
+                cplx_hd = gatedHFreq[slc] . inner(gatedDFreq[slc])  # <h, d>
+                hh = gatedHFreq[slc] . inner(gatedHFreq[slc]).real  # < h, h>
             cplx_loglr = cplx_hd - 0.5*hh
             # store
             setattr(self._current_stats, '{}_optimal_snrsq'.format(det), hh)
