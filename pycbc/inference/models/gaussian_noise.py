b017361f848ca8dd93afb571c305691efb0f1aa8
Shilpa Kastha <shilpakastha@aeiadminsmbp3.localdomain>
2020-10-12 12:45:11 +0200

    added the delay time for the inspiral anaysis

--- pycbc/inference/models/gaussian_noise.py
+++ pycbc/inference/models/gaussian_noise.py
@@ -1224,7 +1224,8 @@ class GatedGaussianNoise(BaseGaussianNoise):
             for F in h.sample_frequencies.numpy():
                 if F <= meco_f:
                     i = i+1
-            gatestartdelay = h.epoch + GateStart[i]
+            gatestart = h.epoch + GateStart[i]
+            gatestartdelay = gatestart + Det.time_delay_from_earth_center(self.current_params['ra'], self.current_params['dec'], gatestart)
             # the kmax of the waveforms may be different than internal kmax
             kmax = min(len(h), self._kmax[det])
             slc = slice(self._kmin[det], kmax)
